d28f5942b8332a2399d07745be278030fbe92edb
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-08-24 13:50:41 +0200

    Add firebase config template

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>',
+    appId: '<your-app-id>',
+    measurementId: '<your-measurement-id>'
+  }
 };
