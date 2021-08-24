69e12ce9a38e2a058f391059a48bb017ab591bf5
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-08-24 20:07:13 +0200

    Remove firestore packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { environment } from '../environments/environment.dev';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NoopAnimationsModule, FormsModule, AngularFireModule.initializeApp(environment.firebase), AngularFirestoreModule],
+  imports: [BrowserModule, NoopAnimationsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: '<your-api-key>',
-    authDomain: '<your-auth-domain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>',
-    appId: '<your-app-id>',
-    measurementId: '<your-measurement-id>'
-  }
+  production: false
 };
