d2c34b8a1ff8aac175a9768385f8514461d84f55
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-08-26 20:34:44 +0200

    Add extract data test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+export const extractData = functions.https.onRequest((request, response) => {
+  const ids: string[] = [];
+  db.listCollections().then((collectionRefs) => {
+    collectionRefs.forEach((collectionRef, index, _) => {
+      ids.push(collectionRef.id);
+      collectionRef.listDocuments().then((docRefs) => {
+        docRefs.forEach((docRef) => {
+          ids.push(docRef.id);
+          docRef.get().then((snapshot) => {
+            if (snapshot.exists) {
+              const data = snapshot.data();
+              if (data) {
+                functions.logger.info(JSON.stringify(data));
+              }
+            }
+          });
+        });
+      });
+    });
+  });
+  response.send("Data extracted " + JSON.stringify(ids));
+});
