2bb61a192a9d41c08cde551804ca2d7d1eb01d3d
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-08-27 09:59:29 +0200

    Fix function deploy errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,32 +10,34 @@ const filePath = path.resolve(__dirname, "..", "export", fileName);
 admin.initializeApp();
 const db = admin.firestore();
 
-export const extractData = functions.https.onRequest(async (request, response) => {
+export const extractData = functions.https.onRequest(async (req, resp) => {
   const workbook = xlsx.utils.book_new();
   const collectionRefs = await db.listCollections();
   for (const collectionRef of collectionRefs) {
     const sheetName = collectionRef.id;
-    const worksheet = xlsx.utils.aoa_to_sheet(await createSheetData(collectionRef));
+    const worksheet = xlsx.utils.aoa_to_sheet(
+      await createSheetData(collectionRef),
+    );
     workbook.SheetNames.push(sheetName);
     workbook.Sheets[sheetName] = worksheet;
   }
   xlsx.writeFile(workbook, filePath);
-  response.sendFile(filePath);
+  resp.sendFile(filePath);
 });
 
-export const testExcelCreation = functions.https.onRequest((request, response) => {
+export const testExcelCreation = functions.https.onRequest((req, resp) => {
   const workbook = xlsx.utils.book_new();
   const sheetName1 = "Sheet1";
   const worksheet1 = xlsx.utils.aoa_to_sheet([
     ["header1", "header2"],
-    ["data1", "data2"]
+    ["data1", "data2"],
   ]);
   workbook.SheetNames.push(sheetName1);
   workbook.Sheets[sheetName1] = worksheet1;
   const sheetName2 = "Sheet2";
   const worksheet2 = xlsx.utils.aoa_to_sheet([
     ["header1", "header2"],
-    ["data1", "data2"]
+    ["data1", "data2"],
   ]);
   workbook.SheetNames.push(sheetName2);
   workbook.Sheets[sheetName2] = worksheet2;
@@ -43,7 +45,14 @@ export const testExcelCreation = functions.https.onRequest((request, response) =
   response.sendFile(filePath);
 });
 
-async function createSheetData(collectionRef: CollectionReference): Promise<string[][]> {
+/**
+ * Creates array of array extracted data for one sheet
+ * @param {CollectionReference} collectionRef firebase reference of one
+ * collection
+ */
+async function createSheetData(
+  collectionRef: CollectionReference,
+): Promise<string[][]> {
   const firstRow = ["Id"];
   const result = [firstRow];
   const docRefs = await collectionRef.listDocuments();
@@ -55,12 +64,15 @@ async function createSheetData(collectionRef: CollectionReference): Promise<stri
       const data = snapshot.data();
       if (data) {
         for (const header in firstRow.slice(1)) {
-          if (data.hasOwnProperty(header)) {
+          if (Object.prototype.hasOwnProperty.call(data, header)) {
             dataRow.push(data[header]);
           }
         }
         for (const key in data) {
-          if (data.hasOwnProperty(key) && !firstRow.includes(key)) {
+          if (
+            Object.prototype.hasOwnProperty.call(data, key) &&
+            !firstRow.includes(key)
+          ) {
             firstRow.push(key);
             dataRow.push(data[key]);
           }
