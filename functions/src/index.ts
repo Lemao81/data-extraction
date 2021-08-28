21fb62d1b9284c68162d58e3f43b58de5220fc3e
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-08-28 20:56:31 +0200

    Implement column sorting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,12 @@ import CollectionReference = admin.firestore.CollectionReference;
 import * as path from "path";
 import * as os from "os";
 
+const sortOrderObj: { [key: string]: string[] } = {
+  collection1: ["field1", "field3", "field2"],
+  collection2: ["field4", "field2"],
+};
+
+const idHeader = "Id";
 const fileName = "extracted-data.xlsx";
 const filePath = path.join(os.tmpdir(), fileName);
 
@@ -13,12 +19,10 @@ const db = admin.firestore();
 
 export const extractData = functions.https.onRequest(async (req, resp) => {
   const workbook = xlsx.utils.book_new();
-  const collectionRefs = await db.listCollections();
-  for (const collectionRef of collectionRefs) {
-    const sheetName = collectionRef.id;
-    const worksheet = xlsx.utils.aoa_to_sheet(
-      await createSheetData(collectionRef),
-    );
+  const collRefs = await db.listCollections();
+  for (const collRef of collRefs) {
+    const sheetName = collRef.id;
+    const worksheet = xlsx.utils.aoa_to_sheet(await createSheetData(collRef));
     workbook.SheetNames.push(sheetName);
     workbook.Sheets[sheetName] = worksheet;
   }
@@ -48,45 +52,117 @@ export const testExcelCreation = functions.https.onRequest((req, resp) => {
 
 /**
  * Creates array of array as extracted data for one sheet
- * @param {CollectionReference} collectionRef firebase reference of one
+ * @param {CollectionReference} collRef firebase reference of one
  * collection
  */
 async function createSheetData(
-  collectionRef: CollectionReference,
+  collRef: CollectionReference,
 ): Promise<string[][]> {
-  const headerRow = ["Id"];
-  const result = [headerRow];
-  const docRefs = await collectionRef.listDocuments();
+  const columnsObj: { [key: string]: string[] } = {
+    Id: [],
+  };
+  const docRefs = await collRef.listDocuments();
+  let docCount = 0;
   for (const docRef of docRefs) {
-    const snapshot = await docRef.get();
-    if (!snapshot.exists) {
+    const docSnapshot = await docRef.get();
+    if (!docSnapshot.exists) {
       continue;
     }
 
-    const data = snapshot.data();
-    if (!data) {
+    const docKeyValues = docSnapshot.data();
+    if (!docKeyValues) {
       continue;
     }
 
-    const dataRow = [snapshot.id];
-    result.push(dataRow);
+    columnsObj.Id.push(docSnapshot.id);
+    for (const key in docKeyValues) {
+      if (!Object.prototype.hasOwnProperty.call(docKeyValues, key)) {
+        continue;
+      }
 
-    headerRow.slice(1).forEach((header) => {
-      if (Object.prototype.hasOwnProperty.call(data, header)) {
-        dataRow.push(data[header]);
+      if (key === idHeader) {
+        throw new Error("'Id' header is reserved");
+      }
+
+      const value = docKeyValues[key];
+      if (key in columnsObj) {
+        columnsObj[key].push(value);
       } else {
-        dataRow.push("");
+        columnsObj[key] = fillWithEmptyString(docCount).concat([value]);
       }
-    });
+    }
 
-    for (const key in data) {
-      if (
-        Object.prototype.hasOwnProperty.call(data, key) &&
-        !headerRow.includes(key)
-      ) {
-        headerRow.push(key);
-        dataRow.push(data[key]);
-      }
+    docCount++;
+  }
+
+  const columns = [];
+  for (const key in columnsObj) {
+    if (!Object.prototype.hasOwnProperty.call(columnsObj, key)) {
+      continue;
+    }
+
+    columns.push([key].concat(columnsObj[key]));
+  }
+
+  const sortOrder = collRef.id in sortOrderObj ? sortOrderObj[collRef.id] : [];
+  const columnsSorted = columns.sort((first, second) =>
+    sortColumns(first, second, sortOrder),
+  );
+
+  return transformToRowsArray(columnsSorted);
+}
+
+function fillWithEmptyString(count: number): string[] {
+  const result = new Array<string>(count);
+  for (let i = 0; i < count; i++) {
+    result[i] = "";
+  }
+
+  return result;
+}
+
+function sortColumns(
+  first: string[],
+  second: string[],
+  sortOrder: string[],
+): number {
+  const firstHeader = first[0];
+  const secondHeader = second[0];
+
+  if (firstHeader === idHeader) {
+    return -1;
+  } else if (secondHeader === idHeader) {
+    return 1;
+  } else if (
+    sortOrder.includes(firstHeader) &&
+    !sortOrder.includes(secondHeader)
+  ) {
+    return -1;
+  } else if (
+    sortOrder.includes(secondHeader) &&
+    !sortOrder.includes(firstHeader)
+  ) {
+    return 1;
+  } else if (
+    sortOrder.includes(firstHeader) &&
+    sortOrder.includes(secondHeader)
+  ) {
+    return sortOrder.indexOf(firstHeader) < sortOrder.indexOf(secondHeader)
+      ? 1
+      : -1;
+  } else {
+    return firstHeader > secondHeader ? 1 : -1;
+  }
+}
+
+function transformToRowsArray(columnsArray: string[][]): string[][] {
+  const rowCount = columnsArray[0].length;
+  const columnCount = columnsArray.length;
+  const result: string[][] = new Array<string[]>(rowCount);
+  for (let i = 0; i < rowCount; i++) {
+    result[i] = new Array<string>(columnCount);
+    for (let j = 0; j < columnCount; j++) {
+      result[i][j] = columnsArray[j][i];
     }
   }
 
