da6a14436b17dddc7f00ba32b560eb4c22587aee
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-08-30 19:55:45 +0200

    Adjust lint rules

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,30 +2,33 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module'
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    '/lib/**/*' // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-  },
+    quotes: ['error', 'single'],
+    'import/no-unresolved': 0,
+    indent: 'off',
+    'max-len': ['error', { code: 160 }],
+    'object-curly-spacing': [2, 'always'],
+    'quote-props': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'never'],
+    'require-jsdoc': 0
+  }
 };
